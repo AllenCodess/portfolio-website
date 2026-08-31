0178baeae528e336423f10531e8e0d31e6ccc2bb
AllenCodess <Aph296@protonmail.com>
2026-08-31 08:56:54 -0400

    restructured the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./index.css";
 import NavBar from "./componenets/NavBar";
+import Hero from "./componenets/Hero";
+import About from "./componenets/About";
 
 function App() {
   return (
     <>
       <NavBar />
+      <Hero />
+      <About />
     </>
   );
 }
---
+++ src/componenets/Hero.jsx
@@ -0,0 +1,26 @@
+const Hero = () => {
+  return (
+    <>
+      <div className="hero-section container">
+        <div className="hero-section-text">
+          <div className="hero-header-container">
+            <h1 className="hero-header">FULL-STACK</h1>
+            <h1 className="hero-header-2">WEB DEVELOPER.</h1>
+          </div>
+          <h4 className="hero-heading">
+            I build secure, scalable, and user friendly web applications for founders and small
+            businesses.
+          </h4>
+          <p className="hero-description">
+            With a background in cybersecurity, I bring a unique perspective to development.
+            Building applications that are not only functional and beautiful, but also secure,
+            reliable, and maintainable.
+          </p>
+          <button className="hero-btn">Download Resume</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Hero;
